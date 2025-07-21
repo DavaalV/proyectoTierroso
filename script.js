b8bdb44ac474467213f0cd6638f8904ebc888780
MatrixAB13 <newdemon90213@gmail.com>
2025-07-21 13:10:55 -0600

    Tu mensaje describiendo el cambio

--- script.js
+++ script.js
@@ -13,6 +13,7 @@ document.addEventListener('DOMContentLoaded', function() {
     link.addEventListener('click', () => {
       navLinks.classList.remove('active');
       menuToggle.querySelector('i').classList.remove('fa-times');
+      menuToggle.querySelector('i').classList.add('fa-bars'); // Asegurar que vuelva al ícono de hamburguesa
     });
   });
   
